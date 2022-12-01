a61d7a3a0996681b9f44023d07d99f6a23f1cea4
AndersonShen <shen.andersonshen@gmail.com>
2022-12-01 17:11:29 +0800

    perf: 將 recommendation-wall 跟 menuToggle 切出去

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+// DOM
+// 產品列表
+const productWrap = document.querySelector(".productWrap");
+// 產品下拉選單
+const productSelect = document.querySelector(".productSelect");
+// 購物車列表
+const cartsList = document.querySelector("#shoppingCart-list");
+// 刪除全部購物車產品
+const deleteCartsBtn = document.querySelector(".discardAllBtn");
+// 表單
+const orderInfoForm = document.querySelector(".orderInfo-form");
+const orderInfoBtn = document.querySelector(".orderInfo-btn");
